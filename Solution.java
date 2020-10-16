1774f7668810f3caf5ce6756df79a3aff042ca2a
MorgothSD <61874453+MorgothSD@users.noreply.github.com>
2020-10-16 18:11:06 +0300

    Add files via upload
    Какое сегодня число?
    Ввести с клавиатуры дату в формате "2013-08-18"
    Вывести на экран введенную дату в виде "AUG 18, 2013".
    Воспользоваться объектом Date и SimpleDateFormat.
    
    
    Требования:
    1. Программа должна считывать данные с клавиатуры.
    2. В программе должна быть объявлена переменная типа SimpleDateFormat.
    3. В программе должна быть объявлена переменная типа Date.
    4. Программа должна выводить данные на экран.
    5. Вывод должен соответствовать заданию.

---
+++ Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task09.task0922;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/* 
+Какое сегодня число?
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sdate = reader.readLine();
+
+        Date date = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(sdate);
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH);
+
+        System.out.println(formatter.format(date).toUpperCase());
+    }
+}
